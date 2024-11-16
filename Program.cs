df8e2cf875f70a0820a2203c618c53caa0e13052
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-16 19:46:15 -0300

    Solving warnings.

--- Program.cs
+++ Program.cs
@@ -2,34 +2,32 @@
 using System.IO;
 using System.Reflection;
 namespace ConsoleFileGeneral {
-   class Program {
-      static void Main() {
-         string sourcePath = @"C:\temp\file.txt";
-         string targetPath = @"C:\temp\file1.txt";
-         try
-         {
-             FileInfo fileInfo = new FileInfo(sourcePath);
-             if (!File.Exists(targetPath))   {
-                 fileInfo.CopyTo(targetPath);
-             }
-             else   {
-                 Console.WriteLine("Arquivo já existe");
-                 return;
-             }
-             string[] lines = File.ReadAllLines(sourcePath);
-             foreach (string line in lines)
-             {
-                 Console.WriteLine(line);
-             }
-         }
-         catch (IOException Ex)
-         {
-             Console.WriteLine("Ocorreu um erro: " + Ex);
-         }
-         finally {
-             Console.WriteLine();
-             Console.WriteLine("Processo realizado com sucesso!");
-         }
-       }    
+    internal abstract class Program {
+        private static void Main() {
+            const string sourcePath = @"C:\temp\file.txt";
+            const string targetPath = @"C:\temp\file1.txt";
+            try {
+                 var fileInfo = new FileInfo(sourcePath);
+                 if (!File.Exists(targetPath))   {
+                     fileInfo.CopyTo(targetPath);
+                 }
+                 else   {
+                     Console.WriteLine("File exists");
+                     return;
+                 }
+                 var lines = File.ReadAllLines(sourcePath);
+                 foreach (var line in lines)
+                 {
+                     Console.WriteLine(line);
+                 }
+            }
+            catch (IOException ex){
+                 Console.WriteLine("Error was found, Message: \n" + ex);
+            }
+            finally {
+                 Console.WriteLine();
+                 Console.WriteLine("Processo realizado com sucesso!");
+            }
+        }    
    }
 }
